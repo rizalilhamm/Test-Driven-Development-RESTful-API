cd62c367be0571fa11db1551780cc58b8745d21e
Rizal Ilham <62590670+rizalilhamm@users.noreply.github.com>
2022-02-15 17:47:16 +0700

    Update settings.py

--- puppy_store/settings.py
+++ puppy_store/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'puppy_store_drf',
         'USER': 'postgres',
-        'PASSWORD': 'helloworld!',
+        'PASSWORD': os.getenv('DBPASSWORD'),
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
